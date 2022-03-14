2fe43da57f8bff691f69e2f8b5abf10f19e58667
didukh_a <annufka94@gmail.com>
2022-03-14 20:52:50 +0200

    попытка добавить стили и аякс

--- bootstrap.php
+++ bootstrap.php
@@ -14,9 +14,9 @@ $isDevMode = false;
 $dbParams = array(
     'driver'   => 'pdo_mysql',
     'host'     => 'database',
-    'user'     => getenv('MYSQL_USER'),
-    'password' => getenv('MYSQL_PASS'),
-    'dbname'   => getenv('MYSQL_DB_NAME'),
+    'user'     => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'dbname'   => getenv('DB_NAME'),
 );
 
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
